ef27eb6cab107a473c70dfd29b5aa397380d1392
Silentneeb <keith_neeb@hotmail.com>
2011-08-11 02:58:48 -0400

    Added missing consolegroup and Ionic.Zlib.dll files.

---
+++ SMP/Groups/ConsoleGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMP
+{
+    public class ConsoleGroup : Group
+    {
+        public ConsoleGroup()
+        {
+			// temp till a better permission node system is in place
+            foreach (Command c in Command.all.All())
+			{
+				this.PermissionList.Add(c.PermissionNode);
+			}
+        }
+    }
+}
