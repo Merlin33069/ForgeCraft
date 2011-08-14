008031a90ef87c207620113bc41e5004eed21071
Jacob Zhitomirsky <bizarrecake@gmail.com>
2011-08-14 09:36:22 -0700

    Modified terrain generator.

--- SMP/World/Generator/GenStandard.cs
+++ SMP/World/Generator/GenStandard.cs
@@ -9,13 +9,38 @@ namespace SMP {
     public class GenStandard : ChunkGen {
         Random random;
         Perlin perlin;
+        Perlin perlin2;
+        Perlin desertSelector;
+        Voronoi voronoi;
+        RidgedMultifractal caves;
 
         public GenStandard() {
             random = new Random();
 
             perlin = new Perlin();
             perlin.Frequency = 0.009;
+            perlin.Persistence = 0.3;
             perlin.Seed = (int)( DateTime.Now.Ticks & 0xffffffff );
+
+            perlin2 = new Perlin();
+            perlin2.Frequency = 0.007;
+            perlin2.Persistence = 0.6;
+            perlin2.Lacunarity = 0.1;
+            perlin2.Seed = perlin.Seed - 7;
+
+            caves = new RidgedMultifractal();
+            caves.Frequency = 0.0089;
+            caves.Seed = perlin2.Seed + 99;
+
+            voronoi = new Voronoi();
+            voronoi.Frequency = 0.008;
+            voronoi.Seed = perlin.Seed + 2;
+
+            desertSelector = new Perlin();
+            desertSelector.Frequency = 0.01;
+            desertSelector.Persistence = 0.4;
+            desertSelector.Lacunarity = 0.14;
+            desertSelector.Seed = perlin.Seed + 245;
         }
 
 
@@ -30,8 +55,8 @@ namespace SMP {
 
             for( int x = 0; x < 16; x++ ) {
                 for( int z = 0; z < 16; z++ ) {
-                    
-                    int v = (int)( 64 + perlin.GetValue( cx + x, 10, cz + z ) * 15 );
+
+                    int v = (int) ( 64 + voronoi.GetValue( cx + x, 5, cz + z ) * 7 + ( perlin2.GetValue( cx + x, 7, cz + z ) + ( perlin.GetValue( cx + x, 10, cz + z ) ) ) * 15 );
 
                     // Bedrock
                     int bedrockHeight = random.Next( 1, 6 );
@@ -44,16 +69,31 @@ namespace SMP {
 
                     // Dirt
                     for( int y = v - 5; y < v; y++ )
-                        c.PlaceBlock( x, y, z, 0x03 );
+                        c.PlaceBlock( x, y, z, (byte)( desertSelector.GetValue( cx + x, y, cz + z ) > 0.5 ? 0x0C : 0x03 ) );
 
 
                     if( v <= waterLevel ) {
                         c.PlaceBlock( x, v, z, 0x0C ); // Send
-                        for( int y = v + 1; y <= waterLevel; y++ )
-                            c.PlaceBlock( x, y, z, 0x08 );
                     }
                     else {
-                        c.PlaceBlock( x, v, z, 0x02 );
+                        c.PlaceBlock( x, v, z, (byte) ( desertSelector.GetValue( cx + x, v, cz + z ) > 0.5 ? 0x0C : 0x02 ) );
+                    }
+                }
+            }
+
+
+            for( int x = 0; x < 16; x++ ) {
+                for( int z = 0; z < 16; z++ ) {
+                    for( int y = 0; y < 128; y++ ) {
+                        if( caves.GetValue( cx + x, y, cz + z ) > ( 128 - y ) * 0.0132 ) {
+                            c.PlaceBlock( x, y, z, 0x00 );
+                            if( c.SGB( x, y - 1, z ) == 0x03 )
+                                c.PlaceBlock( x, y - 1, z, 0x02 );
+                        }
+
+                        if( y <= waterLevel && c.SGB( x, y, z ) == 0x00 ) {
+                            c.PlaceBlock( x, y, z, 0x08 );
+                        }
                     }
                 }
             }
