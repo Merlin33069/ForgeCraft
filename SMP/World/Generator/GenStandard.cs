3fe4b65a8cbb3a9ba630c7e33674b1936ee521b3
Jacob Zhitomirsky <bizarrecake@gmail.com>
2011-08-15 18:32:02 -0700

    Simple mountains added.

--- SMP/World/Generator/GenStandard.cs
+++ SMP/World/Generator/GenStandard.cs
@@ -13,6 +13,8 @@ namespace SMP {
         Perlin desertSelector;
         Voronoi voronoi;
         RidgedMultifractal caves;
+        RidgedMultifractal mountains;
+        Perlin mountains2;
 
         public GenStandard() {
             random = new Random();
@@ -41,6 +43,16 @@ namespace SMP {
             desertSelector.Persistence = 0.4;
             desertSelector.Lacunarity = 0.14;
             desertSelector.Seed = perlin.Seed + 245;
+
+            mountains = new RidgedMultifractal();
+            mountains.Frequency = 0.021;
+            mountains.NoiseQuality = NoiseQuality.Low;
+            mountains.Seed = perlin.Seed + 41;
+
+            mountains2 = new Perlin();
+            mountains2.Frequency = 0.025;
+            mountains2.NoiseQuality = NoiseQuality.Low;
+            mountains2.Seed = perlin.Seed + 41;
         }
 
 
@@ -69,16 +81,49 @@ namespace SMP {
 
                     // Dirt
                     for( int y = v - 5; y < v; y++ )
-                        c.PlaceBlock( x, y, z, (byte)( desertSelector.GetValue( cx + x, y, cz + z ) > 0.5 ? 0x0C : 0x03 ) );
+                        c.PlaceBlock( x, y, z, (byte)( desertSelector.GetValue( cx + x, y, cz + z ) > 0.4 ? 0x0C : 0x03 ) );
 
 
                     if( v <= waterLevel ) {
                         c.PlaceBlock( x, v, z, 0x0C ); // Send
                     }
                     else {
-                        c.PlaceBlock( x, v, z, (byte) ( desertSelector.GetValue( cx + x, v, cz + z ) > 0.5 ? 0x0C : 0x02 ) );
+                        c.PlaceBlock( x, v, z, (byte) ( desertSelector.GetValue( cx + x, v, cz + z ) > 0.35 ? 0x0C : 0x02 ) );
                     }
                 }
+            }
+
+            for( int x = 0; x < 16; x++ ) {
+                //for( int y = 0; y < 128; y++ ) {
+                    for( int z = 0; z < 16; z++ ) {
+                        var d = 0.0;
+                        var prevV = ( 64 + voronoi.GetValue( cx + x, 5, cz + z ) * 7 + ( perlin2.GetValue( cx + x, 7, cz + z ) + ( perlin.GetValue( cx + x, 10, cz + z ) ) ) * 15 );
+                        if( prevV < waterLevel )
+                            d = ( waterLevel - prevV ) * 0.78;
+
+                        var v = ( -mountains.GetValue( cx + x, 127, cz + z ) );
+                        if( v >= d ) {
+                            var h = ( 64 + (int)( v * 39 ) );
+
+                            int lvl = 0;
+
+                            for( int y = h - 1; y >= 0; y-- ) {
+                                var mv = mountains2.GetValue( cx + x, y, cz + z );
+                                bool desert = desertSelector.GetValue( cx + x, y, cz + z ) > 0.4;
+                                //if( mv > 0.4 ) {
+                                    if ( lvl == 0 )
+                                        c.PlaceBlock( x, y, z, (byte)(desert ? 0x0C : 0x02) );
+                                    else if ( lvl < 5 )
+                                        c.PlaceBlock( x, y, z, (byte)(desert ? 0x0C : 0x03) );
+                                    else
+                                        c.PlaceBlock( x, y, z, 0x01 );
+
+                                    ++lvl;
+                                //}
+                            }
+                        }
+                    }
+                //}
             }
 
 
